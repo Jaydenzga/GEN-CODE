8a27da0c87d280c019bc773e1534d3d253791ee3
Jayden Banks <jaydenbanks@MacBookPro.lan>
2026-01-13 16:45:40 -0600

    misspelled variable

--- GEN CODE/bot.py
+++ GEN CODE/bot.py
@@ -3,7 +3,7 @@ import asyncio
 import random
 import discord
 from discord.ext import commands
-from dotevn import load_dotenv
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
